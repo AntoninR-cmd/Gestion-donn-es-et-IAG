400d9ea698a8c91e73c9dbbe5a07453b3d57c493
Antonin Rivron <antonin.rivron@gmail.com>
2026-05-27 10:56:17 +0200

    question 7

--- TD1.py
+++ TD1.py
@@ -5,3 +5,31 @@ Created on Wed May 27 08:55:47 2026
 @author: Antonin
 """
 
+import pandas as pd
+import numpy as np
+import random as rd
+
+countries = pd.read_csv("countries.csv", sep = ";")
+cities = pd.read_csv("cities.csv", sep = ";")
+
+print( countries)
+print(cities)
+
+print(cities.iloc[0:3])
+
+indices = rd.sample(range(len(cities)), 10)
+print(cities.iloc[indices])
+
+print(cities.columns.values)
+
+print(cities.dtypes)
+
+cities.describe()
+
+print(cities.iloc[0:10][["name", "latitude", "longitude"]])
+
+print(cities.iloc[10])
+nom11 = cities.iloc[10]["name"]
+
+liste_nom = cities["name"]
+print(liste_nom.dtypes)
